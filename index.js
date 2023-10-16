98f10bc08da69a7394abc89b627da4e904e86789
m-a-mohsen <ma.abdel.mohsen@gmail.com>
2023-10-16 18:55:23 +0200

    Is there an achievement for this?

--- index.js
+++ index.js
@@ -9,10 +9,30 @@ queryInput.addEventListener("input", (event) => {
 
   const searchString = event.target.value;
 
-  const foundCountry = null;
+  // ------ Part 1 -------------------------
 
-  if (foundCountry) {
-    const countryElement = Country(foundCountry);
-    container.append(countryElement);
+  // const foundCountry = countries.find((item) =>
+  // item.name.toLowerCase().startsWith(searchString.toLowerCase())
+  // );
+
+  // if (foundCountry) {
+  //   const countryElement = Country(foundCountry);
+  //   container.append(countryElement);
+  // }
+
+  // ------ Part 2 -------------------------
+  const foundCountries = countries.filter(
+    (item) => item.name.toLowerCase()[0] === searchString.toLowerCase()[0]
+  );
+    console.log(foundCountries)
+  ;
+
+  //----------------------------------------
+  if (foundCountries) {
+    foundCountries.forEach((item) => {
+      
+      const countryElement = Country(item);
+      container.append(countryElement);
+    })
   }
 });
